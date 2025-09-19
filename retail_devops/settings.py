6a792d7b25070826e8cf1e9711f788d713e75871
zaighumhasaan <Zaighumhassan6@gmail.com>
2025-09-19 16:54:59 +0500

    Fix ALLOWED_HOSTS configuration for Render.com deployment

--- retail_devops/settings.py
+++ retail_devops/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-icees!%g%m0gsn$ozrlj#-en)6f)xo#%b6ha_1ix)02@2a88t4"
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-icees!%g%m0gsn$ozrlj#-en)6f)xo#%b6ha_1ix)02@2a88t4')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['testserver', '127.0.0.1', 'localhost']
+# Allow all hosts for production deployment
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'testserver,127.0.0.1,localhost').split(',')
 
 
 # Application definition
@@ -75,12 +76,19 @@ WSGI_APPLICATION = "retail_devops.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+import dj_database_url
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
